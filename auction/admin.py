8659109b45ec449ea9f2e8f6eb668d51e3787e65
Sharjeel Qadeer <sqadeer@csquareonline.com>
2020-12-02 20:51:50 +0500

    Admin changes

--- auction/admin.py
+++ auction/admin.py
@@ -16,5 +16,22 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ('name', 'auction', 'category')
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', )
+    search_fields = ('name', 'status', )
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'status', )
+    search_fields = ('name', 'address', )
+
+
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ('image', 'status', )
+
+
 admin.site.register(Auction, AuctionAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Attachment, AttachmentAdmin)
