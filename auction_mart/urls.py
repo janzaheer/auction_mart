4a542198839880b27a1bc60be62023a2f846e6d5
SharjeelCC <sharjeel.qadeer@gmail.com>
2020-12-15 13:28:34 +0500

    Authentication improvement (#4)
    Authentication Improvements

--- auction_mart/urls.py
+++ auction_mart/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from common.views import AccountLoginView, AccountSignup, Home
+from common.views import AccountLoginView, Logout, AccountSignup, Home
 
 urlpatterns = [
-    url(r'^login/$', AccountLoginView.as_view(template_name="common/login.html"), name='login'),
+    url(r'^login/$', AccountLoginView.as_view(), name='login'),
+    url(r'^logout/$', Logout.as_view(), name='logout'),
     url(r'^home/$', Home.as_view(template_name="common/home.html"), name='home'),
     url(r'^signup/$', AccountSignup.as_view(template_name="common/signup.html"),  name='signup'),
     url(r'^admin/', admin.site.urls),
--- common/views.py
+++ common/views.py
@@ -1,21 +1,30 @@
-from django.contrib.auth.views import LoginView
-from django.contrib.auth import login, authenticate
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, FormView, TemplateView
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse, reverse_lazy
 
 
 class AccountLoginView(LoginView):
     template_name = 'common/login.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(AccountLoginView, self).get_context_data(**kwargs)
-        context.update({
-            'name': 'Login'
-        })
-        return context
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return super(AccountLoginView, self).dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return HttpResponseRedirect(reverse('home'))
+
+    def form_invalid(self, form):
+        return super(AccountLoginView, self).form_invalid(form)
 
 
 class AccountSignup(FormView):
@@ -39,3 +48,10 @@ class AccountSignup(FormView):
 
 class Home(TemplateView):
     template_name = 'common/home.html'
+
+
+class Logout(LogoutView):
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return HttpResponseRedirect(reverse('login'))
